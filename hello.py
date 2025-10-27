85f07bfc5097cdab231b95733d2d0491f8fc3973
Mathias Depussay <matdepussay@gmail.com>
2025-10-27 16:06:23 +0100

    Envie de pleurer au bout de 5min

--- hello.py
+++ hello.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from projet-webscraping-1!")
+# python
+from bs4 import BeautifulSoup
+import lxml
 
+path = r"c:\Users\matde\Documents\Cours\M2\S1\Projet\Projet_WebScraping\Page HTML\Formula 1 World Championship (1950 - 2024).html"
 
-if __name__ == "__main__":
-    main()
+with open(path, 'r', encoding='utf-8') as file:
+    content = file.read()
+
+soup = BeautifulSoup(content, 'lxml')
+
+print("Verification que la page est parse : " + str(soup)[:200])
+
+def tableau(page ):
+    Tableau_liste = page.find_all('table', class_='table')
+    return Tableau_liste
+
+assert tableau(soup) is not None, "Aucun tableau trouve"
+
+print("Verification des tableaus : " + str(tableau(soup)))
